93ed1c2ae576be31d04109b040163aa4249cc6a3
duyle2002 <duyphi2011@gmail.com>
2025-08-20 22:33:27 +0700

    add jwt entry point

---
+++ src/main/java/duy/com/learnspringboot/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package duy.com.learnspringboot.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import duy.com.learnspringboot.dto.response.ApiResponse;
+import duy.com.learnspringboot.exception.ErrorCode;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(apiResponse));
+
+        response.flushBuffer();
+    }
+}
--- src/main/java/duy/com/learnspringboot/configuration/SecurityConfig.java
+++ src/main/java/duy/com/learnspringboot/configuration/SecurityConfig.java
@@ -48,9 +48,9 @@ public class SecurityConfig {
         httpSecurity.oauth2ResourceServer(oauth2Configurer ->
                 oauth2Configurer.jwt(jwtConfigurer ->
                         jwtConfigurer.decoder(jwtDecoder())
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
-                )
-        );
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+                );
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
         return httpSecurity.build();
